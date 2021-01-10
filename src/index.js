11d9c00ff18b3ba7a335f74b66ecfd1b2deb1e4e
saikeerthi555 <43855677+saikeerthi555@users.noreply.github.com>
2021-01-10 20:07:47 +0530

    wildcard added an asterix and you could've given that particular module a name, so we could call it calculator. And then down here instead of just writing add or multiply, what we have to do instead is we have to add calculator.add, because this calculator is the object and it contains

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { add, subtract, multiply, divide } from "./calculator";
+import * as calculator from "./calculator";
 
 //Import the add, multiply, subtract and divide functions
 //from the calculator.js file.
@@ -8,10 +8,10 @@ import { add, subtract, multiply, divide } from "./calculator";
 
 ReactDOM.render(
   <ul>
-    <li>{add(1, 2)}</li>
-    <li>{multiply(2, 3)}</li>
-    <li>{subtract(7, 2)}</li>
-    <li>{divide(5, 2)}</li>
+    <li>{calculator.add(1, 2)}</li>
+    <li>{calculator.multiply(2, 3)}</li>
+    <li>{calculator.subtract(7, 2)}</li>
+    <li>{calculator.divide(5, 2)}</li>
   </ul>,
   document.getElementById("root")
 );
